379c9cb9b13828a8bb60d5cf8cbd6a7050fb9fea
Sven-Laptop <nevsk@web.de>
2020-05-04 19:11:19 +0200

    update

---
+++ parking_v1/parking_env1.py
@@ -0,0 +1,44 @@
+import gym
+import highway_env
+
+#imports
+from typing import Tuple
+
+from gym.envs.registration import register
+from gym import GoalEnv
+import numpy as np
+from numpy.core._multiarray_umath import ndarray
+
+from highway_env.envs.common.abstract import AbstractEnv
+from highway_env.road.lane import StraightLane, LineType
+from highway_env.road.road import Road, RoadNetwork
+from highway_env.vehicle.kinematics import Vehicle, Obstacle
+
+from highway_env.envs.parking_env import ParkingEnv
+
+
+car_count = 8
+
+class ParkingEnv_1(ParkingEnv):
+    def _create_vehicles(self) -> None:
+        self.vehicle = Vehicle(self.road, [0, 0], 2*np.pi*self.np_random.rand(), 0)
+        self.road.vehicles.append(self.vehicle)
+
+        positions = self.np_random.choice(self.road.network.lanes_list(), size=car_count+1, replace=False)
+
+        for x in range(car_count):
+            lane2 = positions[x]
+            vehicle2 =  Vehicle(self.road, lane2.position(lane2.length/2, 0), lane2.heading, 0)
+            self.road.vehicles.append(vehicle2)
+
+
+        lane = positions[-1]
+        self.goal = Obstacle(self.road, lane.position(lane.length/2, 0), heading=lane.heading)
+        self.goal.COLLISIONS_ENABLED = False
+        self.road.vehicles.insert(0, self.goal)
+
+register(
+    id='parking-v1',
+    entry_point='parking_env1:ParkingEnv_1',
+    max_episode_steps=100
+)
