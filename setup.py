48cc35df634bdf85808ee69b0ea51fda95be1f56
Sven-Laptop <nevsk@web.de>
2020-05-04 20:34:00 +0200

    .

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='highway-env1',
+    version='1.0.dev0',
+    description='parking-v1',
+    url='https://github.com/nevs-k/reinforcement_learning',
+    author='Edouard Leurent',
+    author_email='nevsk@web.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Researchers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+    ],
+
+    keywords='autonomous highway driving simulation environment reinforcement learning',
+    packages=find_packages(exclude=['docs', 'scripts', 'tests']),
+    install_requires=['gym', 'numpy', 'pygame', 'matplotlib', 'pandas'],
+    tests_require=['pytest'],
+    extras_require={
+        'dev': ['scipy'],
+        'deploy': ['pytest-runner', 'sphinx<1.7.3', 'sphinx_rtd_theme']
+    },
+    entry_points={
+        'console_scripts': [],
+    },
+)
