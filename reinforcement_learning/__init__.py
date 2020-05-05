8e99a7a4423ac8b94fff314c586ce40b75c24d70
Sven-Laptop <nevsk@web.de>
2020-05-05 10:19:15 +0200

    update setup.py

---
+++ reinforcement_learning/__init__.py
@@ -0,0 +1,1 @@
+import parking_v1
--- setup.py
+++ setup.py
@@ -1,28 +1,11 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(
-    name='highway_env1',
-    version='1.0.dev0',
-    description='parking-v1',
-    url='https://github.com/nevs-k/reinforcement_learning',
-    author_email='nevsk@web.com',
-    license='MIT',
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Researchers',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.6',
-    ],
-
-    keywords='autonomous highway driving simulation environment reinforcement learning',
-    packages=find_packages(exclude=['docs', 'scripts', 'tests']),
-    install_requires=['gym', 'numpy', 'pygame', 'matplotlib', 'pandas'],
-    tests_require=['pytest'],
-    extras_require={
-        'dev': ['scipy'],
-        'deploy': ['pytest-runner', 'sphinx<1.7.3', 'sphinx_rtd_theme']
-    },
-    entry_points={
-        'console_scripts': [],
-    },
-)
+setup(name='reinforcement_learning',
+      version='0.1',
+      description='parking-v0 with cars',
+      url='https://github.com/nevs-k/reinforcement_learning',
+      author='Sven Knoblauch',
+      author_email='nevsk@web.com',
+      license='MIT',
+      packages=['reinforcement_learning'],
+      zip_safe=False)
