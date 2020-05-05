6a7cc27d49bfa48306d36bd9b3ad4adfe2d69095
Sven-Laptop <nevsk@web.de>
2020-05-05 14:17:48 +0200

    update init

--- reinforcement_learning/parking_env1.py
+++ reinforcement_learning/parking_env1.py
@@ -19,7 +19,7 @@ from highway_env.envs.parking_env import ParkingEnv
 
 car_count = 8
 
-class ParkingEnv_1(ParkingEnv):
+class (ParkingEnv):
     def _create_vehicles(self) -> None:
         self.vehicle = Vehicle(self.road, [0, 0], 2*np.pi*self.np_random.rand(), 0)
         self.road.vehicles.append(self.vehicle)
@@ -32,13 +32,13 @@ class ParkingEnv_1(ParkingEnv):
             self.road.vehicles.append(vehicle2)
 
 
-        lane = positions[-1]
+        lane = positions[-1]ParkingEnv_WithCars
         self.goal = Obstacle(self.road, lane.position(lane.length/2, 0), heading=lane.heading)
         self.goal.COLLISIONS_ENABLED = False
         self.road.vehicles.insert(0, self.goal)
 
 register(
     id='parking-v1',
-    entry_point='reinforcement_learning.parking_env1:ParkingEnv_1',
+    entry_point='reinforcement_learning:ParkingEnv_WithCars',
     max_episode_steps=100
 )
