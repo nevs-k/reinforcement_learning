471cccdfa93e79334e10fd7258aa0d9490104d05
Sven-Laptop <nevsk@web.de>
2020-09-26 12:48:44 +0200

    .

--- reinforcement_learning/envs/parking_env_with_cars.py
+++ reinforcement_learning/envs/parking_env_with_cars.py
@@ -10,6 +10,7 @@ from highway_env.road.lane import StraightLane, LineType
 from highway_env.road.road import Road, RoadNetwork
 from highway_env.vehicle.kinematics import Vehicle, Obstacle
 from highway_env.envs.parking_env import ParkingEnv
+from highway_env.road.objects import Landmark
 
 car_count = 20
 
